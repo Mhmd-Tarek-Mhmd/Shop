131b539c8110b6a60a5f8a927d78f419e9252b29
Mhmd <biboo.biboo97@gmail.com>
2022-07-30 07:41:35 +0200

    ADD: firebase database config

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// Initialize Firebase
+//const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Get a reference to the products collection
+const productsCollection = collection(db, "products");
+
+// Get all products data
+async function getProducts() {
+	const products = [];
+	const productsSnapshot = await getDocs(productsCollection);
+
+	productsSnapshot.forEach((doc) => products.push(doc.data()));
+  return products;
+}
+
+export { getProducts };
