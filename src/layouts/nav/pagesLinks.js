8ef626f53d30c28441bee0dfab8cfef4c4c68488
Mhmd <biboo.biboo97@gmail.com>
2022-08-15 04:32:40 +0200

    ADD: pagesLinks component

---
+++ src/layouts/nav/pagesLinks.js
@@ -0,0 +1,11 @@
+const pages = [];
+
+const PagesLinks = ({ Element, ...props }) => {
+  return pages.map((page) => (
+    <Element key={page} {...props} href={page === "home" ? "/" : `/${page}`}>
+      {page}
+    </Element>
+  ));
+};
+
+export default PagesLinks;
