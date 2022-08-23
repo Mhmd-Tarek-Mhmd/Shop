d900257d7349a02b0875e5a032127eb13c7530a7
Mhmd <biboo.biboo97@gmail.com>
2022-08-23 20:32:13 +0200

    EDIT: firbase db in offline

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  enableIndexedDbPersistence,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 
 // Initialize Firebase
 const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
@@ -8,16 +13,17 @@ const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+enableIndexedDbPersistence(db);
 
 // Get a reference to the products collection
 const productsCollection = collection(db, "products");
 
 // Get all products data
 async function getProducts() {
-	const products = [];
-	const productsSnapshot = await getDocs(productsCollection);
+  const products = [];
+  const productsSnapshot = await getDocs(productsCollection);
 
-	productsSnapshot.forEach((doc) => products.push(doc.data()));
+  productsSnapshot.forEach((doc) => products.push(doc.data()));
   return products;
 }
 
