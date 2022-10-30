614204d06e3934e17b0f78e3dbe58294e84c0c0f
Mhmd <mhmd.tarek197@gmail.com>
2022-10-30 03:01:42 +0200

    ADD: wishlist middleware

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,5 +1,6 @@
 import { cartMiddleware } from "./slices/cart";
+import { wishlistMiddleware } from "./slices/wishlist";
 
-const middleware = [cartMiddleware.middleware];
+const middleware = [cartMiddleware.middleware, wishlistMiddleware.middleware];
 
 export default middleware;
--- src/store/slices/wishlist.js
+++ src/store/slices/wishlist.js
@@ -1,6 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {
+  createListenerMiddleware,
+  createSlice,
+  isAnyOf,
+} from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = sessionStorage.wishlist
+  ? JSON.parse(sessionStorage.wishlist)
+  : [];
 
 const wishlistSlice = createSlice({
   name: "wishlist",
@@ -9,10 +15,25 @@ const wishlistSlice = createSlice({
     addWishlistItem: (state, action) => [...state, action.payload],
     removeWishlistItem: (state, action) =>
       state.filter((a) => a.id !== action.payload),
-    resetWishlist: () => initialState,
+    resetWishlist: () => [],
   },
 });
 
 export default wishlistSlice.reducer;
 export const { addWishlistItem, removeWishlistItem, resetWishlist } =
   wishlistSlice.actions;
+
+export const wishlistMiddleware = createListenerMiddleware();
+wishlistMiddleware.startListening({
+  matcher: isAnyOf(addWishlistItem, removeWishlistItem),
+  effect: (action, listener) => {
+    sessionStorage.setItem(
+      "wishlist",
+      JSON.stringify(listener.getState().wishlist)
+    );
+  },
+});
+wishlistMiddleware.startListening({
+  actionCreator: resetWishlist,
+  effect: () => sessionStorage.removeItem("wishlist"),
+});
