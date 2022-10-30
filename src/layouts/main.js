5354af88e36807970b8d6954f17c554f0a5c3eea
Mhmd <mhmd.tarek197@gmail.com>
2022-10-30 18:12:19 +0200

    EDIT: main layout height

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -4,7 +4,15 @@ import Routes from "../routes";
 
 function Main() {
   return (
-    <Box component="main" sx={{ minHeight: "calc(100% - (64px + 148.02px))" }}>
+    <Box
+      component="main"
+      sx={{
+        minHeight: {
+          xs: "calc(100% - (56px + 140.02px))",
+          sm: "calc(100% - (64px + 148.02px))",
+        },
+      }}
+    >
       <Routes />
     </Box>
   );
