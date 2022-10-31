dc3d485a432a2045a246164c26bbe1533b465e2d
Mhmd <mhmd.tarek197@gmail.com>
2022-11-01 01:08:29 +0200

    EDIT: section components

--- src/components/section.js
+++ src/components/section.js
@@ -2,9 +2,9 @@ import Box from "@mui/material/Box";
 
 import SectionTitle from "./sectionTitle";
 
-function Section({ title, ...props }) {
+function Section({ title, sx, ...props }) {
   return (
-    <Box component="section" sx={{ py: "60px" }} {...props}>
+    <Box component="section" sx={Object.assign({ py: "60px" }, sx)} {...props}>
       {title && <SectionTitle title={title} />}
       {props.children}
     </Box>
--- src/components/sectionTitle.js
+++ src/components/sectionTitle.js
@@ -1,16 +1,19 @@
 import Typography from "@mui/material/Typography";
 
-function SectionTitle({ title, children }) {
+function SectionTitle({ title, sx, children }) {
   return (
     <Typography
       component="h2"
-      sx={{
-        mb: 4,
-        textAlign: "center",
-        letterSpacing: "-3px",
-        textTransform: "uppercase",
-        font: "800 40px/1 sans-serif",
-      }}
+      sx={Object.assign(
+        {
+          mb: 4,
+          textAlign: "center",
+          letterSpacing: "-3px",
+          textTransform: "uppercase",
+          font: "800 40px/1 sans-serif",
+        },
+        sx
+      )}
     >
       {title ? title : children}
     </Typography>
