751ebb32f40542f16e0143defef2b905858d59f6
Mhmd <mhmd.tarek197@gmail.com>
2022-11-27 01:32:09 +0200

    EDIT: uploaded image name

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -2,8 +2,10 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const storage = getStorage();
 
-const getAvatarRef = (uid) => ref(storage, `users/${uid}/avatar.jpg`);
+const getAvatarRef = (uid, fileType) =>
+  ref(storage, `users/${uid}/avatar.${fileType.slice(6)}`);
 
-export const uploadAvatar = (uid, file) => uploadBytes(getAvatarRef(uid), file);
+export const uploadAvatar = (uid, file) =>
+  uploadBytes(getAvatarRef(uid, file.type), file);
 
 export const getAvatarURL = (uid) => getDownloadURL(getAvatarRef(uid));
